544a3944817b64ff07b09e6a34530e30ca2f53ed
shadykh <eng.shady94@gmail.com>
2021-04-04 16:20:48 +0300

    Step3,Finshed

--- server.js
+++ server.js
@@ -45,6 +45,41 @@ function Location(locationData) {
 }
 
 
+server.get('/weather', (req, res) => {
+
+    let weatherData = require('./data/weather.json');
+    weatherData.data.forEach(value => {
+        let renderedWeatherData = new Location(value);
+    });
+
+    res.send(Location.all);
+})
+
+
+function Location(weatherData) {
+
+    /* 
+        [
+            {
+              "forecast": "Partly cloudy until afternoon.",
+              "time": "Mon Jan 01 2001"
+            },
+            {
+              "forecast": "Mostly cloudy in the morning.",
+              "time": "Tue Jan 02 2001"
+            },
+            ...
+          ] */
+
+    this.forecast = weatherData.weather.description;
+
+    this.time = weatherData.datetime;
+
+    Location.all.push(this);
+}
+
+Location.all = [];
+
 
 server.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`)
