1286d32b84c21e3140159bcfd1feebb73686b33e
shadykh <eng.shady94@gmail.com>
2021-04-07 10:41:20 +0300

    lab8, FIX ssd

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const server = express();
 
 const PORT = process.env.PORT || 5330;
 
-const client = new pg.Client(process.env.DATABASE_URL);
-// const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
+// const client = new pg.Client(process.env.DATABASE_URL);
+const client = new Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
 
 
 
